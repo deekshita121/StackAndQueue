251ac53fea2fe15807e6970adf7aeefec3c1b93d
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-14 23:37:16 +0530

    Welcome

---
+++ StackAndQueue/src/main/java/com/capgemini/StackAndQueue/MyNode.java
@@ -0,0 +1,14 @@
+package com.capgemini.StackAndQueue;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MyNode 
+{
+	
+	private static final Logger log = LogManager.getLogger(MyNode.class);
+    public static void main( String[] args )
+    {
+        log.info( "Hello World!" );
+    }
+}
